6308c47cb2878ae641765327427827a60884311a
Eric D. Rohler <erohler@progress.com>
2020-03-29 14:27:08 -0400

    added comments to aliases

--- user_aliases.cmd
+++ user_aliases.cmd
@@ -15,13 +15,14 @@ vi=vim $*
 cmderr=cd /d "%CMDER_ROOT%"
 home=cd /d %USERPROFILE%
 ~=cd /d %USERPROFILE%
+
+rem top-level repos
 repos=cd /d "D:\dev\repos"
 blogging=cd /d "d:\dev\repos\blogging\blogging"
-samples=cd /d "D:\dev\repos\samples"
-templates=cd /d "D:\dev\repos\templates"
 docs=cd /d "D:\dev\repos\docs"
 sources=cd /d "d:\dev\repos\sources"
-msaliases=cd /d "d:\dev\repos\sources\msaliases"
+
+rem support ticket repos
 tix-ajax=cd /d "D:\dev\repos\support\ui-for-aspnet-ajax"
 tix-blazor=cd /d "D:\dev\repos\support\ui-for-blazor"
 tix-core=cd /d "D:\dev\repos\support\ui-for-aspnet-core"
@@ -29,6 +30,8 @@ tix-fiddler=cd /d "D:\dev\repos\support\fiddler"
 tix-mvc=cd /d "D:\dev\repos\support\ui-for-aspnet-mvc"
 tix-rprt=cd /d "D:\dev\repos\support\reporting"
 tix-rprt-srvr=cd /d "D:\dev\repos\support\report-server"
+
+rem docs editing
 docs-ajax=cd /d "D:\dev\repos\docs\ajax-docs"
 docs-blazor=cd /d "d:\dev\repos\docs\blazor-docs"
 docs-seed=cd /d "D:\dev\repos\docs\docs-seed"
@@ -37,5 +40,9 @@ docs-fiddler=cd /d "d:\dev\repos\docs\fiddler-docs"
 docs-fiddler-everywhere=cd /d "d:\dev\repos\docs\fiddler-everywhere-docs"
 docs-rprt=cd /d "D:\dev\repos\docs\reporting-docs"
 docs-rprt-srvr=cd /d "D:\dev\repos\docs\report-server-docs"
+
+rem dev shortcuts
+msa=cd /d "d:\dev\repos\sources\msaliases"
+pb=cd /d "d:\dev\repos\sources\ericrohler_static"
 fed=cd /d "D:\dev\repos\sources\fiddler\Fiddler\WebUi" && code Fiddler.code-workspace && cd /d "D:\dev\repos\sources\fiddler\Fiddler\WebUi\Fiddler.WebUi" && dotnet run bin/Release/netcoreapp3.1/Fiddler.WebUi.dll -c Release
 tdd=cd /d "d:\dev\repos\sources\team-dashboard"
