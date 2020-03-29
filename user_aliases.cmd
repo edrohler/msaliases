57068f730f7c2dd828338fd4958e49f1321b8032
Eric D. Rohler <erohler@progress.com>
2020-03-29 15:00:38 -0400

    updated conemu config

--- user_aliases.cmd
+++ user_aliases.cmd
@@ -16,13 +16,14 @@ cmderr=cd /d "%CMDER_ROOT%"
 home=cd /d %USERPROFILE%
 ~=cd /d %USERPROFILE%
 
-rem top-level repos
+;= rem top-level repos
 repos=cd /d "D:\dev\repos"
 blogging=cd /d "d:\dev\repos\blogging\blogging"
 docs=cd /d "D:\dev\repos\docs"
 sources=cd /d "d:\dev\repos\sources"
+support=cd /d "d:\dev\repos\support"
 
-rem support ticket repos
+;= rem support ticket repos
 tix-ajax=cd /d "D:\dev\repos\support\ui-for-aspnet-ajax"
 tix-blazor=cd /d "D:\dev\repos\support\ui-for-blazor"
 tix-core=cd /d "D:\dev\repos\support\ui-for-aspnet-core"
@@ -31,7 +32,7 @@ tix-mvc=cd /d "D:\dev\repos\support\ui-for-aspnet-mvc"
 tix-rprt=cd /d "D:\dev\repos\support\reporting"
 tix-rprt-srvr=cd /d "D:\dev\repos\support\report-server"
 
-rem docs editing
+;= rem docs editing
 docs-ajax=cd /d "D:\dev\repos\docs\ajax-docs"
 docs-blazor=cd /d "d:\dev\repos\docs\blazor-docs"
 docs-seed=cd /d "D:\dev\repos\docs\docs-seed"
@@ -41,7 +42,7 @@ docs-fiddler-everywhere=cd /d "d:\dev\repos\docs\fiddler-everywhere-docs"
 docs-rprt=cd /d "D:\dev\repos\docs\reporting-docs"
 docs-rprt-srvr=cd /d "D:\dev\repos\docs\report-server-docs"
 
-rem dev shortcuts
+;= rem dev shortcuts
 msa=cd /d "d:\dev\repos\sources\msaliases"
 pb=cd /d "d:\dev\repos\sources\ericrohler_static"
 fed=cd /d "D:\dev\repos\sources\fiddler\Fiddler\WebUi" && code Fiddler.code-workspace && cd /d "D:\dev\repos\sources\fiddler\Fiddler\WebUi\Fiddler.WebUi" && dotnet run bin/Release/netcoreapp3.1/Fiddler.WebUi.dll -c Release
